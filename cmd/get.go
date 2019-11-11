aa4910a6a911653faeb6b5706960a3423f295d5b
dlampsi <dlampsi@gmail.com>
2019-11-11 22:48:25 +0300

    Update cmd by ldap opertaions

--- cmd/get.go
+++ cmd/get.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"cataloger/catalogs/ad"
+	"cataloger/catalogs/ldap"
 	"fmt"
+	"os"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -24,36 +26,14 @@ var (
 		Use:   "user [users_id]",
 		Short: "Get users data from catalog",
 		Run: func(cmd *cobra.Command, args []string) {
-			for _, u := range args {
-				log.Debugf("Get user info: %s\n", u)
-				if source == "ad" {
-					c, err := ad.NewCatalog(createConfig())
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					var data *ad.UserEntry
-					if getShort {
-						data, err = c.Users().GetByAccountNameShort(u)
-					} else {
-						data, err = c.Users().GetByAccountName(u)
-					}
-
-					if err != nil {
-						log.Errorf("ERROR: %s\n", err.Error())
-					}
-					if data == nil {
-						log.Errorf("Entry '%s' not found\n", u)
-					} else {
-						if showOnlyGroups {
-							c.Users().PrintGroups(data)
-						} else if showOnlyGroupsDirect {
-							c.Users().PrintGroupsDirect(data)
-						} else {
-							c.Users().Print(data)
-						}
-					}
-				}
+			switch source {
+			case "ad":
+				getUsersAd(args)
+			case "ldap":
+				getUsersLdap(args)
+			default:
+				log.Errorf("Unknown source '%s'", source)
+				os.Exit(1)
 			}
 		},
 	}
@@ -62,34 +42,13 @@ var (
 		Use:   "group [groups_id]",
 		Short: "Get group data from catalog",
 		Run: func(cmd *cobra.Command, args []string) {
-			for _, g := range args {
-				log.Debugf("Get group info: %s\n", g)
-				if source == "ad" {
-					c, err := ad.NewCatalog(createConfig())
-					if err != nil {
-						log.Fatal(err)
-					}
-					var data *ad.GroupEntry
-					if getShort {
-						data, err = c.Groups().GetByAccountNameShort(g)
-					} else {
-						data, err = c.Groups().GetByAccountName(g)
-					}
-					if err != nil {
-						fmt.Printf("ERROR: %s\n", err.Error())
-					}
-					if data == nil {
-						fmt.Printf("Entry '%s' not found\n", g)
-					} else {
-						if showOnlyMembers {
-							c.Groups().PrintMembers(data)
-						} else if showOnlyMembersDirect {
-							c.Groups().PrintMembersDirect(data)
-						} else {
-							c.Groups().Print(data)
-						}
-					}
-				}
+			switch source {
+			case "ad":
+				getGroupsAd(args)
+			case "ldap":
+				getGroupsLdap(args)
+			default:
+				log.Errorf("Unknown source '%s'", source)
 			}
 		},
 	}
@@ -101,9 +60,123 @@ func init() {
 
 	getCmd.AddCommand(getUserCmd)
 	getUserCmd.Flags().BoolVarP(&showOnlyGroups, "groups", "g", false, "Print only user groups")
-	getUserCmd.Flags().BoolVarP(&showOnlyGroupsDirect, "direct", "d", false, "Print only direct user groups")
+	getUserCmd.Flags().BoolVarP(&showOnlyGroupsDirect, "direct", "d", false, "Print only direct user groups (only AD)")
 
 	getCmd.AddCommand(getGroupCmd)
 	getGroupCmd.Flags().BoolVarP(&showOnlyMembers, "members", "m", false, "Print only group members")
 	getGroupCmd.Flags().BoolVarP(&showOnlyMembersDirect, "direct", "d", false, "Print only direct group members")
+}
+
+func getUsersAd(args []string) {
+	c, err := ad.NewCatalog(createConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, u := range args {
+		log.Debugf("Get user info: %s\n", u)
+		var data *ad.UserEntry
+		if getShort {
+			data, err = c.Users().GetByAccountNameShort(u)
+		} else {
+			data, err = c.Users().GetByAccountName(u)
+		}
+
+		if err != nil {
+			log.Errorf("ERROR: %s\n", err.Error())
+		}
+		if data == nil {
+			log.Errorf("Entry '%s' not found\n", u)
+		} else {
+			if showOnlyGroups {
+				c.Users().PrintGroups(data)
+			} else if showOnlyGroupsDirect {
+				c.Users().PrintGroupsDirect(data)
+			} else {
+				c.Users().Print(data)
+			}
+		}
+	}
+}
+
+func getGroupsAd(args []string) {
+	c, err := ad.NewCatalog(createConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, g := range args {
+		log.Debugf("Get group info: %s\n", g)
+		var data *ad.GroupEntry
+		if getShort {
+			data, err = c.Groups().GetByAccountNameShort(g)
+		} else {
+			data, err = c.Groups().GetByAccountName(g)
+		}
+		if err != nil {
+			fmt.Printf("ERROR: %s\n", err.Error())
+		}
+		if data == nil {
+			fmt.Printf("Entry '%s' not found\n", g)
+		} else {
+			if showOnlyMembers {
+				c.Groups().PrintMembers(data)
+			} else if showOnlyMembersDirect {
+				c.Groups().PrintMembersDirect(data)
+			} else {
+				c.Groups().Print(data)
+			}
+		}
+	}
+}
+
+func getUsersLdap(args []string) {
+	c, err := ldap.NewCatalog(createConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, u := range args {
+		log.Debugf("Get user info: %s\n", u)
+		var data *ldap.UserEntry
+		if getShort {
+			data, err = c.Users().GetByUidShort(u)
+		} else {
+			data, err = c.Users().GetByUid(u)
+		}
+
+		if err != nil {
+			log.Errorf("ERROR: %s\n", err.Error())
+		}
+		if data == nil {
+			log.Errorf("Entry '%s' not found\n", u)
+		} else {
+			if showOnlyGroups {
+				c.Users().PrintGroups(data)
+			} else {
+				c.Users().Print(data)
+			}
+		}
+	}
+}
+
+func getGroupsLdap(args []string) {
+	for _, g := range args {
+		log.Debugf("Get group info: %s\n", g)
+		c, err := ldap.NewCatalog(createConfig())
+		if err != nil {
+			log.Fatal(err)
+		}
+		data, err := c.Groups().GetByCn(g)
+		if err != nil {
+			fmt.Printf("ERROR: %s\n", err.Error())
+		}
+		if data == nil {
+			fmt.Printf("Entry '%s' not found\n", g)
+			continue
+		}
+		if showOnlyMembers {
+			c.Groups().PrintMembers(data)
+		} else {
+			c.Groups().Print(data)
+		}
+	}
 }
--- cmd/rm.go
+++ cmd/rm.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
-	"fmt"
+	"cataloger/catalogs/ldap"
+	"os"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -10,14 +12,48 @@ var (
 	rmCmd = &cobra.Command{
 		Use:   "rm",
 		Short: "Remove entries from catalog",
+	}
+
+	rmGroupCmd = &cobra.Command{
+		Use:   "group",
+		Short: "Remove group entry from catalog",
 		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Printf("%v\n", args)
+			switch source {
+			case "ad":
+				log.Error("Sorry, method not implemented yet")
+				os.Exit(2)
+			case "ldap":
+				rmGroupsLdap(args)
+			default:
+				log.Errorf("Unknown source '%s'", source)
+				os.Exit(1)
+			}
 		},
 	}
 )
 
 func init() {
 	rootCmd.AddCommand(rmCmd)
-	rmCmd.PersistentFlags().String("id", "", "Entry ID attribute")
-	rmCmd.PersistentFlags().String("dn", "", "Entry DN attribute")
+
+	rmCmd.AddCommand(rmGroupCmd)
+}
+
+func rmGroupsLdap(args []string) {
+	c, err := ldap.NewCatalog(createConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, g := range args {
+		log.Debugf("Delete group: %s\n", g)
+		if err := c.Groups().Delete(g); err != nil {
+			switch err {
+			case ldap.ErrAlreadyNotExists:
+				log.Warnf("Group '%s' already not exists", g)
+				os.Exit(2)
+			default:
+				log.Fatal(err)
+			}
+		}
+		log.Infof("Group '%s' deleted", g)
+	}
 }
