0b76dbc7014b2d6baf431941b36190994d522d08
dlampsi <dlampsi@gmail.com>
2020-02-02 22:28:49 +0300

    Fix get relise script

--- scripts/get_darwin_release.sh
+++ scripts/get_darwin_release.sh
@@ -2,7 +2,6 @@
 
 wget https://github.com/dlampsi/cataloger/releases/download/$1/cataloger_$1_darwin_amd64.zip \
 && unzip cataloger_$1_darwin_amd64.zip \
-&& cd cataloger_$1_darwin_amd64 \
 && mv cataloger_darwin_amd64 /usr/local/bin/cataloger \
 && chmod +x /usr/local/bin/cataloger \
 && cd .. && rm -rf cataloger_$1_darwin_amd64*
